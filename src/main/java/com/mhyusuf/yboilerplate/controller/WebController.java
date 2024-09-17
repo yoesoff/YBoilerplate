311bc253e58d23a6d91b07ba913a62e5c7b91d0f
M Yusuf Ibrahim <yusuf.ibrahim@silverlakeaxis.com>
2024-09-17 13:31:06 +0700

    Learn to utilize simple jquery to create basic View and Add to var memory with Thymeleaf

---
+++ src/main/java/com/mhyusuf/yboilerplate/controller/WebController.java
@@ -0,0 +1,11 @@
+package com.mhyusuf.yboilerplate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller public class WebController {
+    @GetMapping(value = "/index")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/mhyusuf/yboilerplate/model/Product.java
@@ -0,0 +1,10 @@
+package com.mhyusuf.yboilerplate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data public class Product {
+    private String id;
+    private String name;
+}
