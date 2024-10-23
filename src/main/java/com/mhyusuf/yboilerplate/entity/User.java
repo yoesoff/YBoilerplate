d804986dc9486be7e8b1a3e1a4d037e3331ae9c3
M Yusuf Ibrahim <yusuf.ibrahim@silverlakeaxis.com>
2024-10-23 14:08:07 +0700

    feat: add Hibernate support and sample implementation
    - Integrated Hibernate with Spring Data JPA
    - Configured database connection in `application.properties`
    - Created `User` entity with JPA annotations
    - Implemented `UserRepository` for CRUD operations
    - Added `UserController` to handle HTTP requests for `User` entity

---
+++ src/main/java/com/mhyusuf/yboilerplate/entity/User.java
@@ -0,0 +1,28 @@
+package com.mhyusuf.yboilerplate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // Getter, Setter, Constructor, dll implemented by Lombok.
+
+}
