a6d41ea0dd23b11af82b1e15c343919455540380
M Yusuf Ibrahim <yusuf.ibrahim@silverlakeaxis.com>
2024-11-28 12:21:54 +0700

    Get By Username

--- src/main/java/com/mhyusuf/yboilerplate/controller/UserController.java
+++ src/main/java/com/mhyusuf/yboilerplate/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mhyusuf.yboilerplate.controller;
 
 import com.mhyusuf.yboilerplate.entity.User;
+import com.mhyusuf.yboilerplate.exception.ResourceNotFoundException;
 import com.mhyusuf.yboilerplate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,12 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
+
+    @GetMapping("/username/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with username " + username));
+    }
+
 }
 
--- src/main/java/com/mhyusuf/yboilerplate/repository/UserRepository.java
+++ src/main/java/com/mhyusuf/yboilerplate/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.mhyusuf.yboilerplate.repository;
 import com.mhyusuf.yboilerplate.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
     // Anda bisa menambahkan query tambahan di sini jika diperlukan
 }
 
