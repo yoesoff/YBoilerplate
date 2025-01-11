727bf379f5055dba017072c5d4047bbca871865c
M Yusuf Ibrahim <yusuf.ibrahim@silverlakeaxis.com>
2025-01-11 14:52:18 +0700

    Move /greeting to base URL to prevent 404 error on base URL

--- src/main/java/com/mhyusuf/yboilerplate/controller/html/GreetingController.java
+++ src/main/java/com/mhyusuf/yboilerplate/controller/html/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    @GetMapping(value = {"/greeting"})
+    @GetMapping(value = {"/"})
     public String greeting(@RequestParam(value = "name", defaultValue = "Yusuf") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
