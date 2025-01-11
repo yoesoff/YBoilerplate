c73fbbaf8e932fcaf3e10ef6d9f76fadbab094cc
M Yusuf Ibrahim <yusuf.ibrahim@silverlakeaxis.com>
2025-01-11 16:39:47 +0700

    Refactor UserController to use UserService
    - Added UserService interface and UserServiceImpl class
    - Updated UserController to use UserService instead of directly accessing UserRepository
    - Added getUserById and deleteUserById methods in UserService and UserServiceImpl
    - Updated UserController to include endpoints for getUserById and deleteUserById

--- src/main/java/com/mhyusuf/yboilerplate/controller/rest/UserController.java
+++ src/main/java/com/mhyusuf/yboilerplate/controller/rest/UserController.java
@@ -1,8 +1,8 @@
-package com.mhyusuf.yboilerplate.controller;
+package com.mhyusuf.yboilerplate.controller.rest;
 
 import com.mhyusuf.yboilerplate.entity.User;
 import com.mhyusuf.yboilerplate.exception.ResourceNotFoundException;
-import com.mhyusuf.yboilerplate.repository.UserRepository;
+import com.mhyusuf.yboilerplate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,23 +13,32 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+        return userService.createUser(user);
     }
 
     @GetMapping("/username/{username}")
     public User getUserByUsername(@PathVariable String username) {
-        return userRepository.findByUsername(username)
+        return userService.getUserByUsername(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with username " + username));
     }
 
-}
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
 
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        userService.deleteUserById(id);
+    }
+}
